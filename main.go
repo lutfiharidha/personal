4ef084266690d32d36fa2f0689f8c28599dc4ea1
Lutfi Haridha <lutfiharidha1@gmail.com>
2021-12-27 02:22:09 +0700

    cors

--- main.go
+++ main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
-	"personal/app/handlers"
+	"os"
 	"personal/configs"
+	"personal/routers"
 
+	"github.com/gin-gonic/gin"
 	"gorm.io/gorm"
 )
 
@@ -11,5 +13,16 @@ var db *gorm.DB = configs.SetupDatabaseConnection()
 
 func main() {
 	defer configs.CloseDatabaseConnection(db)
-	handlers.Routing()
+	r := gin.New()
+	r.Use(configs.CORSMiddleware())
+
+	routers.ClientRoute(r)    //Added all client routes
+	routers.AuthRoute(r)      //Added all auth routes
+	routers.ProfileRoute(r)   //Added all profile routes
+	routers.ContactRoute(r)   //Added all contact routes
+	routers.PortfolioRoute(r) //Added all portfolio routes
+	routers.SkillRoute(r)     //Added all skill routes
+	routers.ContentRoute(r)   //Added all skill routes
+
+	r.Run(":" + os.Getenv("APP_PORT"))
 }
